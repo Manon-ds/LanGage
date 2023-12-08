258772d0be6688833acdb4ffa002c64982bdf06f
Rich B <richardbaker1993@gmail.com>
2023-12-08 15:07:17 +0000

    feat: updated API and added config.ts to gitignore

--- server/config.ts
+++ server/config.ts
@@ -10,6 +10,6 @@
 
 export const port = 3007;
 
-export const apiK = "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";
+export const apiK = "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";
 
 export const deeplApiK = "057cc729-dbb6-3f13-90bf-a43c94ab0cc7:fx";
